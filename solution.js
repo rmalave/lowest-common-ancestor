ffdd5951aa8ebcadbd9a5c68c4f17767aaf474c6
Rafael Malave <rafael.enrique.malave@gmail.com>
2018-06-10 20:10:16 -0700

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,14 +1,13 @@
-function commonAncestor(node, n1, n2) {
-  if (!node) return;
-
-  let val = node.value;
-
-  if (n1 < val && n2 < val) {
-    return commonAncestor(node.left, n1, n2);
+function commonAncestorBT(node, n1, n2){
+   if(!node) return;
+   var val = node.value;
+   if(n1 == val || n2 ==val){
+     return node;
+   }
+   var left = commonAncestorBT(node.left, n1, n2);
+   var right = commonAncestorBT(node.right, n1, n2);
+   if(left && right){
+     return node;
   }
-  if (n1 < val && n2 < val) {
-    return commonAncestor(node.right, n1, n2);
-  }
-
-  return node;
+  return (left) ? left : right;
 }
